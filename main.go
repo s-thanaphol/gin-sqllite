db5bbcf37d92410000c131876ff51cbddc3dc15e
Thanaphol Ovatvoravarunyou <thanaphol.ovat@gmail.com>
2023-06-06 18:30:45 +0700

    canupdate

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
+
+	"example.com/personweb/models"
 )
 
 func main() {
+
+	err := models.ConnectDatabase()
+	checkErr(err)
 	r := gin.Default()
 
 	// API v1
@@ -22,4 +32,89 @@ func main() {
 	// PORT environment variable was defined.
 	r.Run()
 
+}
+
+func getPersons(c *gin.Context) {
+	persons, err := models.GetPersons(10)
+	checkErr(err)
+
+	if persons == nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "No Records Found"})
+		return
+	} else {
+		c.JSON(http.StatusOK, gin.H{"data": persons})
+	}
+}
+
+func getPersonById(c *gin.Context) {
+
+	id := c.Param("id")
+
+	person, err := models.GetPersonById(id)
+	checkErr(err)
+	// if the name is blank we can assume nothing is found
+	if person.FirstName == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "No Records Found"})
+		return
+	} else {
+		c.JSON(http.StatusOK, gin.H{"data": person})
+	}
+}
+
+func addPerson(c *gin.Context) {
+
+	var json models.Person
+
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	success, err := models.AddPerson(json)
+
+	if success {
+		c.JSON(http.StatusOK, gin.H{"message": "Success"})
+	} else {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err})
+	}
+}
+
+func updatePerson(c *gin.Context) {
+
+	var json models.Person
+
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	personId, err := strconv.Atoi(c.Param("id"))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+	}
+
+	success, err := models.UpdatePerson(json, personId)
+
+	if success {
+		c.JSON(http.StatusOK, gin.H{"message": "Success"})
+	} else {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err})
+		fmt.Println("Hello World!4444")
+	}
+}
+
+func deletePerson(c *gin.Context) {
+	id := c.Param("id")
+	c.JSON(http.StatusOK, gin.H{"message": "deletePerson " + id + " Called"})
+}
+
+func options(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "options Called"})
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
